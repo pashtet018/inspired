ca8821e5872998c7539aa8c177e8bd2ecdefe79e
pashtet018 <121239662+pashtet018@users.noreply.github.com>
2023-01-14 03:00:44 +0300

    Add files via upload

---
+++ src/modules/mainPage/mainPage.js
@@ -0,0 +1,13 @@
+import { renderHero } from "../render/renderHero"
+import { renderNavigation } from "../render/renderNavigation"
+import { renderProducts } from "../render/renderProducts"
+
+
+
+export const mainPage = (gender = 'women') => {
+    console.log('gender: ', gender);
+    renderNavigation (gender)
+    renderHero (gender)
+    renderProducts (gender)
+    
+}
---
+++ src/modules/render/renderHero.js
@@ -0,0 +1,47 @@
+import { createElement } from "../createElement";
+
+export const renderHero = (gender) => {
+    const hero =document.querySelector('.hero');
+    if(!gender){
+        hero.style.display = 'none';
+        return;
+    }
+    hero.style.display = '';
+
+    hero.className = `hero hero__${gender}` 
+
+    const container = createElement('div', {
+        className: 'container'
+    },
+    {
+        parent: hero,
+    });
+    const genderContent = createElement('div', {
+        className: 'hero__content'
+    },
+    {
+        parent: container,
+    });
+    const genderTitle = createElement('h2', {
+        className: 'hero__title'
+    },
+    {
+        parent: genderContent,
+    });
+    const genderLink = createElement('a', {
+        className: 'hero__link'
+    },
+    {
+        parent: genderTitle,
+    });
+
+    hero.innerHTML = `
+    <div class="container">
+      <div class="hero__content">
+        <h2 class="hero__title">Новая коллекция Бюстгальтер-балконет</h2>
+
+        <a href="#" class="hero__link">Перейти</a>
+      </div>
+    </div>
+    `;
+}
