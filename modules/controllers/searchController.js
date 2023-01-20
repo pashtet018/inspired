88886f0cc4fc2192330512ef17fc01e275b21823
pashtet018 <121239662+pashtet018@users.noreply.github.com>
2023-01-20 11:39:12 +0300

    Delete modules directory

--- modules/controllers/searchController.js
+++
@@ -1,8 +0,0 @@
-import { router } from "../router";
-
-export const searchController = formSearch => {
-    formSearch.addEventListener('submit', (e) => {
-        e.preventDefault();
-        router.navigate(`search?value=${formSearch.search.value}`)
-    })
-}
