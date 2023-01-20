7f441a44ea449ce0ab5e577f227526aa430e7673
pashtet018 <121239662+pashtet018@users.noreply.github.com>
2023-01-20 11:37:08 +0300

    Add files via upload

---
+++ modules/const.js
@@ -0,0 +1,13 @@
+export const COUNT__PAGINATION = 3;
+export const DATA = {};
+export const API_URL = 'http://localhost:8024';
+export const TITLE = {
+    women: {
+        title: 'Новая коллекция Бюстгальтер-балконет ', 
+        id: '2269903069'
+    },
+    men: {
+        title: 'Боксеры из новой коллекции', 
+        id: '4413664778'
+    }
+}
---
+++ modules/render/renderProducts.js
@@ -0,0 +1,100 @@
+import { API_URL, COUNT__PAGINATION, DATA } from "../const";
+import { createElement } from "../createElement";
+import { getData } from "../getData";
+import { renderPagination } from "./renderPagination";
+
+export const renderProducts = async (title, params) => {
+    const products = document.querySelector('.goods');
+    products.textContent = '';
+
+    const data = await getData(`${API_URL}/api/goods`, params);
+
+    const goods = Array.isArray(data) ? data : data.goods;
+    
+    const container = createElement('div', {
+        className: 'container',
+    }, {
+        parent: products
+    });
+    
+    createElement('h2', {
+        className: 'goods__title',
+        textContent: title,
+    }, {
+        parent: container
+    },);
+
+    const listCard = goods.map((product) => {
+        const li = createElement('li', {
+            className: 'goods__item',
+        });
+
+
+    const article = createElement('article', {
+        className: 'product',
+            innerHTML: `
+          <a href="#/product/${product.id}" class="product__link">
+            <img 
+            src="${API_URL}/${product.pic}" 
+            alt="${product.title}" 
+            class="product__image">
+            <h3 class="product__title">${product.title}</h3>
+          </a>
+
+          <div class="product__row">
+            <p class="product__price">руб ${product.price}</p>
+
+            <button 
+            class="product__btn-favorite" 
+            aria-label="добавить в избранное"
+            data-id=${product.id}></button>
+          </div>
+        `,
+        }, {
+            parent: li
+        },);
+
+        createElement('ul', {
+            className: 'product__color-list',
+        },
+        {
+            parent: article, 
+            appends: product.colors.map((colorId, i) => {
+                
+                const color = DATA.colors.find((item) => item.id == colorId);
+
+                return createElement('li', {
+                    className: `color color_${color.title} ${i ? '' : 'color_check'}`
+                },
+                );
+            })
+        },);
+
+        
+
+        return li;
+    },);
+
+    createElement(
+    'ul', 
+    {
+        className: 'goods__list',
+    }, 
+    {
+        appends: listCard,
+        parent: container,
+    },
+    );
+    
+    if( data.pages && data.pages>1) {
+        const pagination = createElement('div', {
+            className: 'goods__pagination pagination'
+        },
+        {
+            parent: container,
+        })
+
+
+        renderPagination(pagination, data.page, data.pages, COUNT__PAGINATION)
+    }
+};
